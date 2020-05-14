aa2b9d10d482463d6cfd22a795e96a09aa71cea9
Sergio Tribaldos <sergio.tribaldos@rindus.de>
2020-05-14 19:32:29 +0200

    Back (Constants): Added constants

--- back/src/shared/constants.ts
+++ back/src/shared/constants.ts
@@ -1,9 +1,11 @@
 export const ERROR_MESSAGES = {
   DUPLICATED_MAIL: 'Duplicated mail',
+  PRODUCT_UPLOAD_FAIL: 'Product upload failed',
 };
 
 export const SUCCESFUL_MESSAGES = {
   REGISTER: 'Register succesful',
+  PRODUCT_UPLOAD: 'Product uploaded succesfully',
 };
 
 export const RESPONSE_STATUS = {
