19c3f15189c16016a04cc4f6d4fcc41fbe327234
Sergio Tribaldos <sergio.tribaldos@rindus.de>
2020-05-14 19:27:23 +0200

    Feat (Shared store): Shared store deleted

--- front/src/app/shared/store/shared-actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const SHARED_ACTIONS = {
-  showNotification: createAction(
-    '[Any ] Show notification',
-    props<{ msg: string; msgType: string }>()
-  ),
-};
