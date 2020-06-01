bad326fa0da2ff919995824f2cc54207d060877f
Sergio Tribaldos <sergio.tribaldos@rindus.de>
2020-06-01 13:13:27 +0200

    Feat (app): Minor implementations

--- front/src/app/shared/user-info-header/user-info-header.component.ts
+++ front/src/app/shared/user-info-header/user-info-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/auth/model/user';
+import { Product } from 'src/app/home/product /model/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-info-header',
@@ -7,9 +9,16 @@ import { User } from 'src/app/auth/model/user';
   styleUrls: ['./user-info-header.component.css'],
 })
 export class UserInfoHeaderComponent implements OnInit {
-  @Input()
-  user: User;
-  constructor() {}
+  @Input() user: User;
+  @Input() product: Product;
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  goToChat() {
+    this.router.navigate(['user-menu/chat'], {
+      queryParams: { productUserId: this.user.id, productId: this.product.id },
+    });
+  }
 }
